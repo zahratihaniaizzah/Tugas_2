cdce845862ced74d7d7d4bc103b2ac087da58351
zahratihaniaizzah <zahratihaniaizzah@gmail.com>
2026-07-30 09:50:55 +0800

    yyap

--- Studi Kasus 3.js
+++ Studi Kasus 3.js
@@ -1,2 +1,26 @@
-const platKendaraan = [
-    { plat: "B 1234 ABC", jenis: "Mobil" },                 
+let platKendaraan = [
+    "DD1234AB",
+    "DD5678CD",
+    "DD9013EF",
+    "DD4562GH",
+    "DD7895IJ"
+];
+function filterParkirGanjilGenap(data, tanggal) {
+
+    let jenisTanggal = (tanggal % 2 == 0) ? "Genap" : "Ganjil";
+
+    console.log("Tanggal hari ini: " + tanggal);
+    console.log("Jenis tanggal: " + jenisTanggal);
+
+    for (let i = 0; i < data.length; i++) {
+
+        let angka = data[i][5];
+        let jenisPlat = (angka % 2 == 0) ? "Genap" : "Ganjil";
+
+        if (jenisPlat == jenisTanggal) {
+            console.log(data[i] + " boleh masuk");
+        }
+    }
+}
+
+filterParkirGanjilGenap(platKendaraan, 15);
