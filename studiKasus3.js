192dbf487f4525c149b355acdb2b590fa10e91d2
zahratihaniaizzah <zahratihaniaizzah@gmail.com>
2026-08-05 09:34:52 +0800

    siapjadiJeanGrey

--- studiKasus3.js
+++ studiKasus3.js
@@ -10,29 +10,29 @@ function filterParkirGanjilGenap(dataPlat, tanggal) {
 
     let jenisTanggal;
 
-    if (tanggal % 2 == 0) {
+    if (tanggal % 2 === 0) {
         jenisTanggal = "Genap";
     } else {
         jenisTanggal = "Ganjil";
     }
 
-    console.log("Tanggal hari ini : " + tanggal);
-    console.log("Jenis tanggal    : " + jenisTanggal);
-    console.log("Kendaraan yang boleh masuk:");
+    console.log(`Tanggal hari ini : ${tanggal}`);
+    console.log(`Jenis tanggal    : ${jenisTanggal}`);
+    console.log(`Kendaraan yang boleh masuk:`);
 
     for (let i = 0; i < dataPlat.length; i++) {
 
         let angka = dataPlat[i][5];
         let jenisPlat;
 
-        if (angka % 2 == 0) {
+        if (angka % 2 === 0) {
             jenisPlat = "Genap";
         } else {
             jenisPlat = "Ganjil";
         }
 
-        if (jenisPlat == jenisTanggal) {
-            console.log(dataPlat[i] + " (" + jenisPlat + ") boleh masuk");
+        if (jenisPlat === jenisTanggal) {
+            console.log(`${dataPlat[i]} (${jenisPlat}) boleh masuk`);
         }
     }
 }
